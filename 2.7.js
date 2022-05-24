ced571487b3ccabcc315f1b8c5f6b0e6a7eea375
Lydia <lydia.haway@gmail.com>
2022-05-24 17:24:36 +0200

    secondex

--- 2.7.js
+++ 2.7.js
@@ -1,3 +1,20 @@
 const readlineSync = require("readline-sync");
 
-let n = readlineSync.question("Could you give a number?");
+let n = readlineSync.question("Can you give us a number ? ");
+
+let val = [];
+let sum = 0;
+let i = 0;
+
+for (i = 0; i < n; i++) {
+  let askAgain = readlineSync.question("Can you give us a new number ? ");
+  val.push(askAgain);
+}
+
+let sumVal = val.map(Number);
+
+for (i = 0; i < sumVal.length; i++) {
+  sum += sumVal[i];
+}
+
+console.log(`The sum is ${sum}`);
