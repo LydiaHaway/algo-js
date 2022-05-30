9fc0928d1616b06816b7ea33ec7adc326530c914
Lydia <lydia.haway@gmail.com>
2022-05-30 10:12:37 +0200

    ex

---
+++ 6.4.js
@@ -0,0 +1,25 @@
+const readlineSync = require("readline-sync");
+
+class Human {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  get Name() {
+    return `${this.firstName} ${this.lastName} `;
+  }
+
+  set Name(val) {
+    this.firstName = val.split(" ")[0];
+    this.lastName = val.split(" ")[1];
+  }
+}
+
+let obj1 = new Human("Sherlock", "Holmes");
+
+console.log(obj1.Name); // Sherlock Holmes
+
+obj1.Name = "Mycroft Holmes";
+
+console.log(obj1.firstName); // Mycroft
