096a3b3795a2c5415e163d6267f303e727c8a7b9
Lydia <lydia.haway@gmail.com>
2022-05-30 11:11:09 +0200

    ex

--- 5.1.js
+++ 5.1.js
@@ -3,12 +3,20 @@ const readlineSync = require("readline-sync");
 function askTvSerie() {
   let name = readlineSync.question("what's your favorite Tv Show?");
   let time = readlineSync.question("What was the year of its production?");
-  let cast = readlineSync.question("who was in the main cast?");
+  let cast = [];
+
+  do {
+    let castAsk = readlineSync.question("Who was in the main cast");
+    cast.push(castAsk);
+    askAgain = readlineSync.question("Wanna add another actors?");
+  } while (askAgain === "yes");
+
+  let castList = cast.toString();
 
   const result = {
     "TV Show": name,
     "Years of production": time,
-    "cast members": cast,
+    "cast members": castList,
   };
 
   return result;
--- 5.2.js
+++ 5.2.js
@@ -1,3 +1,25 @@
 const readlineSync = require("readline-sync");
 
-function randomizeCast() {}
+function askTvSerie() {
+  let name = readlineSync.question("what's your favorite Tv Show?");
+  let time = readlineSync.question("What was the year of its production?");
+  let cast = [];
+
+  do {
+    let castAsk = readlineSync.question("Who was in the main cast");
+    cast.push(castAsk);
+    askAgain = readlineSync.question("Wanna add another actors?");
+  } while (askAgain === "yes");
+
+  let castList = cast.toString();
+
+  const tvSerie = {
+    "TV Show": name,
+    "Years of production": time,
+    "cast members": castList,
+  };
+
+  return tvSerie;
+}
+
+function randomizeCast(tvSerie) {}
